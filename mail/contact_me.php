4dfee8cb752e803aa511cdaf2dd3474bae5a0c78
Preeti Gondi <pgondi@andrew.cmu.edu>
2015-07-26 14:17:44 -0400

    Testing out email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'pgondi@andrew.cmu.edu';
+$to = 'preeti.gondi@gmail.com;
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@gmail.com\n"; 
